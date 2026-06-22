696229c6d516bdf20bb33e00837999ae9aea5bad
ssannssarr <ssantanusarkar8@gmail.com>
2026-06-22 23:06:53 +0530

    feat: add install.sh script for cloning and installing Utilitis package

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "clonning repository Utilitis..."
+git clone https://github.com/ssannssarr/Utilitis
+echo done clonning
+sleep 3
+echo Installing Utilitis package globaly
+pip install ./Utilitis
+echo DONE!! 
